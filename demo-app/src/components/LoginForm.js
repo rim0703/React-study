77318b38570dadcddb99aba63a2989ddd493a3d1
rim0703 <rim0703@163.com>
2022-06-28 23:37:00 +0900

    Login Page Layout basic

---
+++ demo-app/src/components/LoginForm.js
@@ -0,0 +1,100 @@
+//client/src/components/LoginForm.js
+ 
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Collapse from "react-bootstrap/Collapse";
+import {Col, Row} from "react-bootstrap";
+
+import ThemeProvider from 'react-bootstrap/ThemeProvider' // 정렬 
+import logo from '../logo.svg';
+
+
+const LoginForm = (props) => {
+  return (
+
+    
+    <div
+        style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '15vh',
+        }}
+    >
+
+    <Form> 
+
+
+      {/* 인증번호 화면이 뜰지 안뜰지 설정 / props.showAuthCode가 true이면 안보입니다. */}
+      {/* <Collapse in={!props.showAuthCode}> */}
+          
+        <Form.Group>
+        
+        <Row>
+        <Col>
+            <div>
+            <img src={logo} className="App-logo" alt="logo" />
+            </div>
+        </Col>
+
+        <Col>
+        <Row>
+          <Form.Label>아이디</Form.Label>
+          <Col>
+            <Form.Control
+                type="text"
+                placeholder=""
+                value={props.ID}
+                onChange={props.handleID}
+            />
+          </Col>
+          </Row>
+
+          <br></br>
+
+          <Row>
+            <Form.Label>비밀번호</Form.Label>
+            <Col>
+            <Form.Control
+                type="password"
+                // placeholder="Password"
+                value={props.password}
+                onChange={props.handlePassword}
+            />
+            </Col>
+          </Row>
+
+          <br></br>
+          <Button variant="primary" type="submit">
+            로그인
+          </Button>
+          </Col>
+          </Row>
+        </Form.Group>
+
+
+      {/* </Collapse> */}
+      {/* 인증번호 화면이 뜰지 안뜰지 설정 / props.showAuthCode가 true이면 나타납니다.
+      <Collapse in={props.showAuthCode}>
+        <Form.Group>
+          <Form.Label>Authentification Code</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder=""
+            value={props.code}
+            onChange={props.handleCode}
+          />
+          <Button variant="primary" type="submit">
+            Confirm
+          </Button>
+        </Form.Group>
+      </Collapse> */}
+    </Form>
+
+    </div>
+
+  );
+};
+ 
+export default LoginForm;
---
+++ demo-app/src/pages/Login.js
@@ -0,0 +1,47 @@
+//client/src/pages/Login.js
+import React, { useState } from "react";
+import LoginForm from "../components/LoginForm";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+ 
+const Login = () => {
+  const [ID, setID] = useState("");
+  const [password, setPassword] = useState("");
+//   const [code, setCode] = useState("");
+ 
+  const handleID = (e) => {
+    setID(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+//   const handleCode = (e) => {
+//     setCode(e.target.value);
+//   };
+  const handleSubmit = (e) => {
+  };
+ 
+  return (
+    <Row>
+      <Col xs={0} md={0}></Col>
+      <Col xs={0} md={5}>
+        <Card body style={{ marginTop: "20rem", borderRadius: "10px" }}>
+            <center>
+          <h3>관리자 페이지에 로그인하세요</h3></center>
+          <LoginForm
+            ID={ID}
+            password={password}
+            // showAuthCode={false}
+            handleID={handleID}
+            handlePasswo={handlePassword}
+            handleSubmit={handleSubmit}
+            // handleCode={handleCode}
+          />
+        </Card>
+      </Col>
+      <Col xs={0} md={0}></Col>
+    </Row>
+  );
+};
+export default Login;
