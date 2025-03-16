5676e500c58e9cc2f8355ff9906f10c758a6fac8
rim0703 <rim0703@163.com>
2025-03-16 20:45:41 +0900

    [Feature] 중앙부 로고 구현
    - 중안 로고 구현

--- React/2-disney-plus/src/App.jsx
+++ React/2-disney-plus/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./Component/Header";
 import "./App.css";
 import Slider from "./Component/Slider";
+import ProductionHouse from "./Component/ProductionHouse";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +12,7 @@ function App() {
       <div>
         <Header />
         <Slider />
+        <ProductionHouse />
       </div>
     </>
   );
